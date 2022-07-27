73b0a6ade98d8537cc75330029c9fce9ba4b9d18
dev1-pcp <mahmudamen@gmail.com>
2022-07-27 12:55:32 +0200

    first commit

---
+++ models/stock_quant.py
@@ -0,0 +1,50 @@
+# ?? 2015-2017 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
+from odoo.tools import config, float_compare
+
+
+class StockQuant(models.Model):
+    _inherit = "stock.quant"
+
+    @api.constrains("product_id", "quantity")
+    def check_negative_qty(self):
+        p = self.env["decimal.precision"].precision_get("Product Unit of Measure")
+        check_negative_qty = (
+            config["test_enable"] and self.env.context.get("test_stock_no_negative")
+        ) or not config["test_enable"]
+        if not check_negative_qty:
+            return
+
+        for quant in self:
+            disallowed_by_product = (
+                not quant.product_id.allow_negative_stock
+                and not quant.product_id.categ_id.allow_negative_stock
+            )
+            disallowed_by_location = not quant.location_id.allow_negative_stock
+            if (
+                float_compare(quant.quantity, 0, precision_digits=p) == -1
+                and quant.product_id.type == "product"
+                and quant.location_id.usage in ["internal", "transit"]
+                and disallowed_by_product
+                and disallowed_by_location
+            ):
+                msg_add = ""
+                if quant.lot_id:
+                    msg_add = _(" lot '%s'") % quant.lot_id.name_get()[0][1]
+                raise ValidationError(
+                    _(
+                                                   "لا يوجد رصيد كافي لعملية المخزون هذه لأن مستوى مخزون  "
+                                                  "المنتج '%s'%s سيصبح سالبًا (%s) في موقع المخزون '%s' و "
+                                                       "ولا يُسمح بالمخزون السلبي لهذا المنتج و / أو الموقع.."
+                    )
+                    % (
+                        quant.product_id.display_name,
+                        msg_add,
+                        quant.quantity,
+                        quant.location_id.complete_name,
+                    )
+                )
